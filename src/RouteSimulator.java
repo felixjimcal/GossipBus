24e7672dc362d8e61e66c4f91d7e506c212469c9
unknown <felixcalvo@gmail.com>
2019-01-09 11:06:50 -0800

    Signed-off-by: felixjimcal <felixcalvo@gmail.com>

---
+++ src/RouteSimulator.java
@@ -0,0 +1,13 @@
+public class RouteSimulator {
+
+    BusDriver driverA = new BusDriver("A");
+    BusDriver driverB = new BusDriver("B");
+
+    Route route_BCN = new Route();
+
+    // TODO
+    //  Add list of drivers
+    // route_BCN.addDrivers();
+
+    route.StartRoute();
+}
