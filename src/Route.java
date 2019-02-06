48e9794266e7aec465ecbbc62a7c1e866e74f644
felixjimcal <felixjcalvo@gmail.com>
2019-02-06 21:28:39 +0100

    Signed-off-by: felixjimcal <felixjcalvo@gmail.com>

--- src/Route.java
+++ src/Route.java
@@ -4,15 +4,15 @@ class Route {
 
     private int work_minutes = 480;
     Set<Integer> route_stops = new HashSet<>();
-    Set<Character> drivers_gossips = new HashSet<>();
-    private List<BusDriver> drivers = new ArrayList<>();
+    Set<Character> all_gossips = new HashSet<>();
+    private List<BusDriver> drivers_on_the_route = new ArrayList<>();
     private Set<BusDriver> drivers_on_the_same_stop = new HashSet<>();
     Set<Character> gossips_from_drivers_on_the_same_stop = new HashSet<Character>();
 
     int total_stops = 0;
 
     Route(BusDriver... drivers) {
-        this.drivers.addAll(Arrays.asList(drivers));
+        this.drivers_on_the_route.addAll(Arrays.asList(drivers));
 
         this.CollectStops();
         this.CollectGossips();
@@ -20,32 +20,41 @@ class Route {
     }
 
     private void CollectStops() {
-        for (BusDriver my_driver : drivers) {
+        for (BusDriver my_driver : drivers_on_the_route) {
             route_stops.addAll(my_driver.m_stops);
         }
     }
 
     private void CollectGossips() {
-        for (BusDriver my_driver : drivers) {
-            drivers_gossips.addAll(my_driver.gossips);
+        for (BusDriver my_driver : drivers_on_the_route) {
+            all_gossips.addAll(my_driver.gossips);
         }
     }
 
     private void CollectDriversOnTheSameStop() {
         List<Integer> stops_list = new ArrayList<>(route_stops);
 
-        for (int j = 0; j < work_minutes; j++) {
-            for (int i = 0; i < route_stops.size(); i++) {
-                for (BusDriver driver : drivers) {
-                    if (driver.m_stops.get(i).equals(stops_list.get(i))) {
+        for (int minutes = 0; minutes < work_minutes; minutes++) {
+            for (int bus_stops = 0; bus_stops < route_stops.size(); bus_stops++) {
+                for (BusDriver driver : drivers_on_the_route) {
+                    if (driver.m_stops.get(bus_stops).equals(stops_list.get(bus_stops))) {
                         drivers_on_the_same_stop.add(driver);
                     }
                 }
+                // No estan entrando los conductores con la misma parada...
                 if (drivers_on_the_same_stop.size() > 1) {
                     CollectGossipsFromCollectedDrivers();
                 }
                 drivers_on_the_same_stop.clear();
                 total_stops++;
+
+                if (AllGossipsAreShared()) {
+                    System.out.println("Se acabo");
+                }
+                else
+                {
+                    System.out.println("Total stops:" + total_stops);
+                }
             }
         }
     }
@@ -56,21 +65,16 @@ class Route {
         }
 
         ExchangeGossips();
-
-        if (AllGossipsAreShared()) {
-            return;
-        }
     }
 
     private boolean AllGossipsAreShared() {
         boolean result = false;
 
-        for (BusDriver driver : drivers) {
-            if (driver.gossips.size() == drivers_gossips.size()) {
+        for (BusDriver driver : drivers_on_the_route) {
+            if (driver.gossips.size() == all_gossips.size()) {
                 result = true;
             }
         }
-
         return result;
     }
 
--- src/RouteShould.java
+++ src/RouteShould.java
@@ -17,15 +17,14 @@ public class RouteShould {
     @Before
     public void intitialize() {
         driverA = new BusDriver('A', 1, 3, 1);
-        driverB = new BusDriver('B', 1, 3, 2);
-        driverC = new BusDriver('C', 3, 2);
+        driverB = new BusDriver('B', 3, 2, 1);
+       // driverC = new BusDriver('C', 3, 2);
 
-        route_BCN = new Route(driverA, driverB, driverC);
-
+        route_BCN = new Route(driverA, driverB);
     }
 
     @Test
-    public void GetTotalStops() {
+    public void GetTotalStopsInTheRoute() {
         int total_stops_need = route_BCN.route_stops.size();
 
         Assert.assertThat(total_stops_need, is(3));
@@ -35,20 +34,29 @@ public class RouteShould {
     public void CheckStopsCollected() {
         Set<Integer> stops_expected = route_BCN.route_stops;
 
-        Assert.assertThat(stops_expected, is(new HashSet<>(Arrays.asList(1, 3, 2))));
+        Assert.assertThat(stops_expected, is(new HashSet<>(Arrays.asList(1, 2, 3))));
     }
 
     @Test
     public void CheckGossips() {
-        Set<Character> gossips_expected = route_BCN.drivers_gossips;
+        Set<Character> gossips_expected = route_BCN.all_gossips;
 
-        Assert.assertThat(gossips_expected, is(new HashSet<>(Arrays.asList('A', 'B', 'C'))));
+        Assert.assertThat(gossips_expected, is(new HashSet<>(Arrays.asList('A', 'B'))));
     }
 
+    @Test
+    public void TotalStopsToShareAllTheGossips() {
+        int total_stops_need = route_BCN.total_stops;
+
+        Assert.assertThat(total_stops_need, is(2));
+    }
+
+    /*
     @Test
     public void CheckGossipsFromCollectedBusDrivers()
     {
         Set<Character> gossips_expected = route_BCN.gossips_from_drivers_on_the_same_stop;
         Assert.assertThat(gossips_expected, is(new HashSet<>(Arrays.asList('A','B', 'C'))));
     }
+    */
 }
