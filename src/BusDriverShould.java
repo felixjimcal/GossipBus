0b382226c02851ca437347041d34f8836f93f57d
felixjimcal <felixjcalvo@gmail.com>
2019-02-18 21:45:40 +0100

    Signed-off-by: felixjimcal <felixjcalvo@gmail.com>

--- src/BusDriverShould.java
+++ src/BusDriverShould.java
@@ -1,22 +1,32 @@
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.hamcrest.core.Is.is;
 
+@RunWith(JUnitParamsRunner.class)
 public class BusDriverShould {
+
+    private BusDriver driver;
 
     @Before
     public void Initialize()
     {
-
+         driver = new BusDriver('A', 5,1,4,3);
     }
 
     @Test
-    public void CheckIfNextStopIsWhatTheStopWeWant()
+    @Parameters({
+            "5",
+            "1",
+            "4",
+            "3"})
+    public void CheckIfNextStopIsWhatTheStopWeWant(int val)
     {
-        BusDriver driver = new BusDriver('A', 5,1,4,3);
         Integer expected_station = driver.NextStop();
-        Assert.assertThat(expected_station, is(5));
+        Assert.assertThat(expected_station, is(val));
     }
 }
